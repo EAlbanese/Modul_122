d3fc3772c47493f5a34e59c8c25461b7bd62d7b3
EAlbanese <eliaalbanese@icloud.com>
2022-06-17 15:24:59 +0200

    wip

--- bin/Script1.sh
+++ bin/Script1.sh
@@ -1,1 +1,46 @@
+#!/bin/bash
+cwd=`pwd`        # current working directory
+cd `dirname $0`    # change to the directory where the script is located
+BINDIR=`pwd`    # BINDIR: the directory where the script is located
+cd $cwd        # return to the working directory
+BASENAME=`basename $0`    # Set the script name (without path to it)
+TMPDIR=/tmp/$BASENAME.$$    # Set a temporary directory if needed
+ETCDIR=$BINDIR/../etc        # ETCDIR is the config directory
 
+#Jetzt sollte als erstes das/die Konfigfile(s) eingelesen werden:
+# .    # run config file “Scriptname”.env
+userList=()
+groupList=()
+
+echo ""
+echo "USERS"
+echo ""
+
+if [ -s ../etc/user.txt ];
+    then 
+    cat ../etc/user.txt |grep -v '^#'|grep -v '^$' | while read user firstname name group ; do
+        userList+=($user)
+        userList+=($firstname)
+        userList+=($name)
+        userList+=($group)
+        echo ${userList[@]}
+        echo "---------------------"    
+    done
+    
+else
+    echo "File is empty"
+fi
+
+echo ""
+echo "GROUP"
+echo ""
+
+if [ -s ../etc/group.txt ];
+    then 
+    cat ../etc/group.txt |grep -v '^#'|grep -v '^$' | while read user ; do
+        echo groupname: $user
+        echo "---------------------"
+    done
+else
+    echo "File is empty"
+fi
